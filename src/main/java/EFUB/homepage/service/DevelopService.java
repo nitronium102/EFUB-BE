b55829993fd922030e38e2b3e68b9410cf737566
hoit1302 <jueun000101@gmail.com>
2021-11-27 00:17:49 +0900

    feat(DevelopService): add get develop res dto method

--- src/main/java/EFUB/homepage/service/DevelopService.java
+++ src/main/java/EFUB/homepage/service/DevelopService.java
@@ -1,18 +1,26 @@
 package EFUB.homepage.service;
 
 import EFUB.homepage.domain.Develop;
-import EFUB.homepage.dto.DevelopDto;
-import EFUB.homepage.dto.UpdateDevelopDto;
+import EFUB.homepage.domain.Interview;
+import EFUB.homepage.domain.Tool;
+import EFUB.homepage.dto.*;
 import EFUB.homepage.repository.DevelopRepository;
+import EFUB.homepage.repository.InterviewRepository;
+import EFUB.homepage.repository.ToolRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class DevelopService {
     private final DevelopRepository developRepository;
+    private final ToolRepository toolRepository;
+    private final InterviewRepository interviewRepository;
 
     @Transactional
     public Develop save(DevelopDto developDto){
@@ -31,5 +39,18 @@ public class DevelopService {
                 updateDevelopDto.getExp(),
                 updateDevelopDto.getLink(),
                 updateDevelopDto.getOrientation());
+    }
+
+    @Transactional(readOnly = true)
+    public DevelopResDto getDevelop(Long user_id) {
+        Optional<Develop> develop = developRepository.findByUserId(user_id);
+        if (develop.isEmpty())
+            return (new Develop()).toDevelopResDto(null, null);
+
+        List<ToolResDto> tools = toolRepository.findByUserId(user_id)
+            .stream().map(Tool::toToolResDto).collect(Collectors.toList());
+        List<InterviewResDto> interviews = interviewRepository.findByUserId(user_id)
+            .stream().map(Interview::toInterviewResDto).collect(Collectors.toList());
+        return develop.get().toDevelopResDto(tools, interviews);
     }
 }
