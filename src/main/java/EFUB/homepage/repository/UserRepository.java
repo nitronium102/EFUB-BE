268840a9bd32ae3645642d10b8561e9ae0660cf1
seojunglee <71055448+seojunglee@users.noreply.github.com>
2021-11-27 08:00:54 +0900

    style() : removed comments

--- src/main/java/EFUB/homepage/repository/UserRepository.java
+++ src/main/java/EFUB/homepage/repository/UserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-//    Boolean existsByNameAndPhoneNoAndPassword(String name, String phoneNo, String password);
     User findByNameAndPhoneNoAndPassword(String name, String phoneNo, String password);
-
 }
