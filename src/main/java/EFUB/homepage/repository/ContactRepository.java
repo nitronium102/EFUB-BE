db49599b962550a531bee4ccbfd1a8eaf4e96d69
Minji Kang <nitronium@ewhain.net>
2021-11-27 11:06:25 +0900

    feat(ContactRepository): add repository annotation

--- src/main/java/EFUB/homepage/repository/ContactRepository.java
+++ src/main/java/EFUB/homepage/repository/ContactRepository.java
@@ -2,9 +2,10 @@ package EFUB.homepage.repository;
 
 import EFUB.homepage.domain.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-	List<Contact> findByContactId(Long contactId);
+	Contact findByContactId(Long contactId);
 }
