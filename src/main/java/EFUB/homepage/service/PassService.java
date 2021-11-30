d5a3931702d1eb420a70b8d8f19490dbe3ddd9ad
Minji Kang <nitronium@ewhain.net>
2021-11-30 16:31:56 +0900

    feat(PassService): add password checking logic

--- src/main/java/EFUB/homepage/service/PassService.java
+++ src/main/java/EFUB/homepage/service/PassService.java
@@ -7,30 +7,32 @@ import EFUB.homepage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 
 @RequiredArgsConstructor
 @Service
 public class PassService {
 	private final UserRepository userRepository;
 
-	public PassResponseDto checkMidPass(PassDto passDto){
+	public PassResponseDto checkMidPass(PassDto passDto) {
 
-		User user = userRepository.findByNameAndPhoneNoAndPassword(
+		User user = userRepository.findByNameAndPhoneNo(
 			passDto.getName(),
-			passDto.getPhoneNo(),
-			passDto.getPassword()
+			passDto.getPhone_no()
 		);
 
 		Boolean isPass;
 		String result;
 
-		if (user != null){
-			isPass = user.getPassMid();
-		} else {
-			result = "잘못된 정보입니다.";
-			return new PassResponseDto(result);
+		if (user == null) { // 등록된 유저가 없는 경우
+			return new PassResponseDto("잘못된 정보입니다.");
+		}
+		if (!Objects.equals(user.getPassword(), passDto.getPassword())) {
+			return new PassResponseDto("비밀번호가 틀립니다.");
 		}
 
+		isPass = user.getPassMid();
 		if (isPass)
 			result = "합격";
 		else
@@ -39,24 +41,24 @@ public class PassService {
 		return new PassResponseDto(result);
 	}
 
-	public PassResponseDto checkFinPass(PassDto passDto){
+	public PassResponseDto checkFinPass(PassDto passDto) {
 
-		User user = userRepository.findByNameAndPhoneNoAndPassword(
+		User user = userRepository.findByNameAndPhoneNo(
 			passDto.getName(),
-			passDto.getPhoneNo(),
-			passDto.getPassword()
+			passDto.getPhone_no()
 		);
 
 		Boolean isPass;
 		String result;
 
-		if (user != null){
-			isPass = user.getPassFinal();
-		} else {
-			result = "잘못된 정보입니다.";
-			return new PassResponseDto(result);
+		if (user == null) { // 등록된 유저가 없는 경우
+			return new PassResponseDto("잘못된 정보입니다.");
+		}
+		if (!Objects.equals(user.getPassword(), passDto.getPassword())) {
+			return new PassResponseDto("비밀번호가 틀립니다.");
 		}
 
+		isPass = user.getPassFinal();
 		if (isPass)
 			result = "합격";
 		else
