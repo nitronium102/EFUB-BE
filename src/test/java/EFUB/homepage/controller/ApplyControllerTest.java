a91cfaef0ec79527b40e2e60a3fb4fd2ebad934e
hyoji530 <dkakfl56@gmail.com>
2021-12-03 03:04:10 +0900

    test(ApplyController): change objectMapper

--- src/test/java/EFUB/homepage/controller/ApplyControllerTest.java
+++ src/test/java/EFUB/homepage/controller/ApplyControllerTest.java
@@ -1,9 +1,6 @@
 package EFUB.homepage.controller;
 
-import EFUB.homepage.dto.SaveDesignDto;
-import EFUB.homepage.dto.SaveDevelopDto;
-import EFUB.homepage.dto.SaveInterviewDto;
-import EFUB.homepage.dto.SaveToolDto;
+import EFUB.homepage.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -40,6 +34,9 @@ class ApplyControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp(){
@@ -63,7 +60,7 @@ class ApplyControllerTest {
         interviewList.add(interview2);
 
         SaveDevelopDto newDevelop = SaveDevelopDto.builder()
-                .user_id(Long.valueOf(100))
+                .user_id(Long.valueOf(1000))
                 .motive("지원동기 테스트")
                 .project_topic("하고싶은 주제 테스트")
                 .application_field("리드개발자-테스트")
@@ -87,7 +84,7 @@ class ApplyControllerTest {
         toolList.add(tool2);
 
         SaveDesignDto newDesign = SaveDesignDto.builder()
-                .user_id(Long.valueOf(101))
+                .user_id(Long.valueOf(1001))
                 .motive("지원동기")
                 .confidence_des(4)
                 .tool(toolList)
@@ -109,7 +106,7 @@ class ApplyControllerTest {
 
         mvc.perform(MockMvcRequestBuilders.post("/api/recruitment/apply/save/dev")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(newDevelop)))
+                .content(objectMapper.writeValueAsString(newDevelop)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
@@ -120,7 +117,7 @@ class ApplyControllerTest {
 
         mvc.perform(MockMvcRequestBuilders.post("/api/recruitment/apply/save/des")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(newDesign)))
+                .content(objectMapper.writeValueAsString(newDesign)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
@@ -131,10 +128,12 @@ class ApplyControllerTest {
 
         mvc.perform(MockMvcRequestBuilders.post("/api/recruitment/apply/save/dev")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(newDevelop))
+                .content(objectMapper.writeValueAsString(newDevelop))
                 .param("save_final", "true"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    //TODO: add update test
 
 
 }
