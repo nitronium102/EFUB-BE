7ed93b4fe1c8f181e0afd0513c98178da4a0a20a
Jueun <jueun000101@gmail.com>
2022-01-26 18:57:11 +0900

    feat(User, UserService, UserReqDto): add saving user logic

--- src/main/java/EFUB/homepage/service/UserService.java
+++ src/main/java/EFUB/homepage/service/UserService.java
@@ -1,11 +1,26 @@
 package EFUB.homepage.service;
 
+import EFUB.homepage.domain.Position;
+import EFUB.homepage.domain.User;
+import EFUB.homepage.dto.common.UserReqDto;
 import EFUB.homepage.repository.UserRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+
+	public User save(UserReqDto user, Position position) {
+		return userRepository.save(
+				User.builder()
+						.name(user.getName())
+						.password(user.getPassword())
+						.phoneNo(user.getPhoneNo())
+						.studentId(user.getStudentId())
+						.department(user.getDepartment())
+						.position(position)
+						.build());
+	}
 }
