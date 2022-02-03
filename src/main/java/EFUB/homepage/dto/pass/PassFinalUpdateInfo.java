92db0b172c704ac37052139f96f530f884a3cd0f
Jueun <jueun000101@gmail.com>
2022-02-03 13:06:56 +0900

    feat(PassFinalUpdateInfo, PassMidUpdateInfo): add update passMid and passFinal dtos

---
+++ src/main/java/EFUB/homepage/dto/pass/PassFinalUpdateInfo.java
@@ -0,0 +1,9 @@
+package EFUB.homepage.dto.pass;
+
+import lombok.Getter;
+
+@Getter
+public class PassFinalUpdateInfo {
+	private Long userId;
+	private Boolean passFinal;
+}
---
+++ src/main/java/EFUB/homepage/dto/pass/PassMidUpdateInfo.java
@@ -0,0 +1,9 @@
+package EFUB.homepage.dto.pass;
+
+import lombok.Getter;
+
+@Getter
+public class PassMidUpdateInfo {
+	private Long userId;
+	private Boolean passMid;
+}
