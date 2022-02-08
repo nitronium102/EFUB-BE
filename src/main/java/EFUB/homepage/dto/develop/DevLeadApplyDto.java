8a9c3092373fed11b475cf73115cb4309643de9c
Jueun <jueun000101@gmail.com>
2022-02-08 23:45:36 +0900

    feat(Dev...ApplyDto, Dev...ReqDto, DesApplyDto): update dtos to specify position

--- src/main/java/EFUB/homepage/dto/develop/DevLeadApplyDto.java
+++ src/main/java/EFUB/homepage/dto/develop/DevLeadApplyDto.java
@@ -2,17 +2,26 @@ package EFUB.homepage.dto.develop;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
-public class DevApplyDto {
+public class DevLeadApplyDto {
+	@NotBlank
 	private String motive;
+	@NotBlank
 	private String projectTopic;
+	@NotBlank
 	private String applicationField;
 	private String language;
 	private Integer confidenceLang;
 	private String link;
+	@NotNull
 	private Boolean orientation;
+	@NotBlank
 	private String expProject;
+	@NotBlank
 	private String expSolve;
+	@NotBlank
 	private String seminarTopic;
-	private String expStudy;
 }
